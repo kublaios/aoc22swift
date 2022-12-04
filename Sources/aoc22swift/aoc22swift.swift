cc0ef296bc5782ddcd55568f2d7db1e6c7a7760b
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-12-04 22:00:56 +0100

    Update TODO.

--- Sources/aoc22swift/aoc22swift.swift
+++ Sources/aoc22swift/aoc22swift.swift
@@ -1,7 +1,7 @@
 @main
 public struct aoc22swift {
     public static func main() {
-        // TODO: Refactor this to use command line arguments
+        // TODO: Maybe refactor this to use command line arguments
         let solution = Day4.Solution()
         print("Solution Part One: \(solution.partOne())")
         print("Solution Part Two: \(solution.partTwo())")
