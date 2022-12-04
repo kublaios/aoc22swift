ef282974f20ffa521dee25a60d294e02aca0085f
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-12-04 20:06:34 +0100

    Add boilerplate for day 4.

---
+++ Tests/aoc22swiftTests/day4Tests.swift
@@ -0,0 +1,17 @@
+//
+// Created by Kubilay Erdogan on 2022-12-04.
+//
+
+import XCTest
+@testable import aoc22swift
+
+final class day4Tests: XCTestCase {
+    func testSolutionInitializes() {
+        let solution = Day4.Solution()
+        XCTAssertNotNil(solution)
+    }
+
+    // MARK: Day one
+
+    // MARK: Day two
+}
