1c35c2ddbdfc4bb6c87c38de1ea99af87177739b
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-12-05 08:38:40 +0100

    Add boilerplate for day 5.

---
+++ Sources/aoc22swift/day5/Day5Solution.swift
@@ -0,0 +1,11 @@
+//
+// Created by Kubilay Erdogan on 2022-12-05.
+//
+
+import Foundation
+
+enum Day5 {
+    struct Solution{
+
+    }
+}
---
+++ Tests/aoc22swiftTests/day5Tests.swift
@@ -0,0 +1,13 @@
+//
+// Created by Kubilay Erdogan on 2022-12-05.
+//
+
+import XCTest
+@testable import aoc22swift
+
+final class day5Tests: XCTestCase {
+    func testSolutionInit() {
+        let solution = Day5.Solution()
+        XCTAssertNotNil(solution)
+    }
+}
