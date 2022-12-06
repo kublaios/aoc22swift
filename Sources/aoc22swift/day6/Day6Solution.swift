961b057ac2b4139e88150ae1c2255485e7061684
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-12-06 09:39:49 +0100

    Add boilerplate for day 6.

---
+++ Sources/aoc22swift/day6/Day6Solution.swift
@@ -0,0 +1,13 @@
+//
+// Created by Kubilay Erdogan on 2022-12-06.
+//
+
+import Foundation
+
+enum Day6 {
+    struct Solution {
+        func partOne() -> String {
+            "Not implemented"
+        }
+    }
+}
---
+++ Tests/aoc22swiftTests/day6Tests.swift
@@ -0,0 +1,17 @@
+//
+// Created by Kubilay Erdogan on 2022-12-05.
+//
+
+import XCTest
+@testable import aoc22swift
+
+final class day6Tests: XCTestCase {
+    // MARK: Part one
+
+    func testSolutionPartOne() {
+        let solution = Day6.Solution()
+        let result = solution.partOne()
+        XCTAssertEqual(result, "Not implemented")
+    }
+
+}
