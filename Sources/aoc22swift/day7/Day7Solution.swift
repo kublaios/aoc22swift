5204be662ce424461851530e071da2952998f9ff
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-12-08 23:54:07 +0100

    Day 7 solution part 1 (brutal!).

--- Sources/aoc22swift/day7/Day7Solution.swift
+++ Sources/aoc22swift/day7/Day7Solution.swift
@@ -4,7 +4,135 @@
 
 import Foundation
 
+protocol StorageUnit {}
+
 enum Day7 {
     struct Solution {
+        let inputProvider: InputProvider
+
+        init(inputProvider: InputProvider = InputProvider(file: #file)) {
+            self.inputProvider = inputProvider
+        }
+
+        func partOne() -> Int {
+            let allSizes = FlatFileTreeBuilder.build(input: inputProvider.input)
+            // Sum of the sizes below 100000
+            return allSizes.values.filter { $0 <= 100000 }.reduce(0, +)
+        }
+    }
+}
+
+extension Day7 {
+    struct File: StorageUnit {
+        let name: String
+        let size: Int
+    }
+
+    struct Directory: StorageUnit {
+        let name: String
+        let size: Int
+        let fullPath: String
+        var children: [StorageUnit]
+    }
+
+    enum FlatFileTreeBuilder {
+        static func build(input: String) -> [String: Int] {
+            let lines = input.split(separator: "\n")
+            var currentDirectory = ""
+            var listing = false
+            var flatTree: [String: Directory] = [:]
+
+            for line in lines {
+                if line.hasPrefix("$") {
+                    // stop saving output for the listing if we receive a new command
+                    listing = false
+                }
+                if line.hasPrefix("$ cd ") {
+                    // build the full path if we cd into a directory and create dictionary entry if it doesn't exist
+                    let direction = line.replacingOccurrences(of: "$ cd ", with: "")
+                    if direction == "/" {
+                        currentDirectory = "/"
+                    } else if direction == ".." {
+                        currentDirectory = currentDirectory.components(separatedBy: "/").dropLast(2).joined(separator: "/") + "/"
+                    } else {
+                        currentDirectory += direction + "/"
+                    }
+                    if flatTree[currentDirectory] == nil {
+                        flatTree[currentDirectory] = Directory(name: direction, size: 0, fullPath: currentDirectory, children: [])
+                    }
+                } else if line.hasPrefix("$ ls") {
+                    // start saving output for the listing
+                    listing = true
+                } else if listing {
+                    // save the output for the listing
+                    guard let currentUnit = flatTree[currentDirectory] else {
+                        // sanity check
+                        fatalError("No current unit")
+                    }
+                    let meta = line.components(separatedBy: " ")
+                    let name = meta.last!
+                    var children = currentUnit.children
+                    if line.hasPrefix("dir ") {
+                        let fullPath = currentDirectory + name + "/"
+                        // Add current item to the children of the current directory
+                        children.append(Directory(name: name, size: 0, fullPath: fullPath, children: []))
+                        flatTree[currentDirectory]?.children = children
+                    } else {
+                        let size = Int(meta[0])!
+                        // Add current item to the children of the current directory
+                        children.append(File(name: name, size: size))
+                        flatTree[currentDirectory]?.children = children
+                    }
+                }
+            }
+
+            // Correct the children of root elements
+            flatTree = FlatTreeCorrector(flatTree: flatTree).correct()
+            var sizesCalculated: [String: Int] = [:]
+            let calculator = SizeCalculator(flatTree: flatTree)
+            for (path, directory) in flatTree {
+                sizesCalculated[path] = calculator.calculateSize(of: directory)
+            }
+            return sizesCalculated
+        }
+    }
+
+    struct FlatTreeCorrector {
+        let flatTree: [String: Directory]
+
+        /// Loops through the root elements and attach them to their parents
+        func correct() -> [String: Directory] {
+            var correctedTree: [String: Directory] = flatTree
+            for (path, directory) in flatTree {
+                var children = directory.children
+                for (index, child) in children.enumerated() {
+                    if let child = child as? Directory,
+                       let childDirectory = flatTree[child.fullPath] {
+                        children[index] = childDirectory
+                    }
+                }
+                correctedTree[path]?.children = children
+            }
+            return correctedTree
+        }
+    }
+
+    struct SizeCalculator {
+        let flatTree: [String: Directory]
+
+        /// Calculates sizes of the directory by using flatTree as lookup table
+        func calculateSize(of directory: Directory) -> Int {
+            var size = 0
+            if let dir = flatTree[directory.fullPath] {
+                for child in dir.children {
+                    if let child = child as? Directory {
+                        size += calculateSize(of: child)
+                    } else if let child = child as? File {
+                        size += child.size
+                    }
+                }
+            }
+            return size
+        }
     }
 }
--- Tests/aoc22swiftTests/day7Tests.swift
+++ Tests/aoc22swiftTests/day7Tests.swift
@@ -6,5 +6,35 @@ import XCTest
 @testable import aoc22swift
 
 final class Day7Tests: XCTestCase {
+    let input = """
+                $ cd /
+                $ ls
+                dir a
+                14848514 b.txt
+                8504156 c.dat
+                dir d
+                $ cd a
+                $ ls
+                dir e
+                29116 f
+                2557 g
+                62596 h.lst
+                $ cd e
+                $ ls
+                584 i
+                $ cd ..
+                $ cd ..
+                $ cd d
+                $ ls
+                4060174 j
+                8033020 d.log
+                5626152 d.ext
+                7214296 k
+                """
 
+    func testSolutionPartOne() {
+        let expected = 95437
+        let result = Day7.Solution(inputProvider: InputProvider(rawInput: input)).partOne()
+        XCTAssertEqual(result, expected)
+    }
 }
