46026679f14bc08f92e8025e1c9000a5f51a21d8
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2022-12-08 09:20:16 +0100

    Add boilerplate for day 7.

---
+++ Sources/aoc22swift/day7/Day7Solution.swift
@@ -0,0 +1,10 @@
+//
+// Created by Kubilay Erdogan on 2022-12-08.
+//
+
+import Foundation
+
+enum Day7 {
+    struct Solution {
+    }
+}
---
+++ Tests/aoc22swiftTests/day7Tests.swift
@@ -0,0 +1,10 @@
+//
+// Created by Kubilay Erdogan on 2022-12-08.
+//
+
+import XCTest
+@testable import aoc22swift
+
+final class Day7Tests: XCTestCase {
+
+}
